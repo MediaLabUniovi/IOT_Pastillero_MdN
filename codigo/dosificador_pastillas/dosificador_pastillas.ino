9038444b58f9a35543cee0290ba64b4f37e8f804
miguelinente <miguelenterria@gmail.com>
2025-08-05 08:29:12 +0200

    modo dormido funcional

--- codigo/dosificador_pastillas/dosificador_pastillas.ino
+++ codigo/dosificador_pastillas/dosificador_pastillas.ino
@@ -13,29 +13,10 @@ void setup() {
   Serial.begin(115200);
   peripherals_setup(); // RTC, botón, Buzzer y LEDs
   server_config();
-  print_wakeup_reason();
+  //print_wakeup_reason();
   esp_sleep_wakeup_cause_t wakeup_reason;
   wakeup_reason = esp_sleep_get_wakeup_cause();
-  if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0){ // Esto ocurrirá cuando quiera ponerlo en modo config o al parar la alarma
-    t_pulsado = millis();
-    while (digitalRead(BUTTON) == LOW){ //cutre pero funciona
-      if (millis() - t_pulsado >= 3000){ // Se mantiene el botón pulsado por 3 segundos
-        modoConfig();
-        WiFi_setup();
-        config = true;
-        timeoutCP.start();
-      }
-    } 
-    Serial.printf("valor de intFlag: %d\n", intFlag);
-    if (!config && !intFlag){ // Solo si no ha entrado en modo configuracion. intflag no debería de ser true porque se habría puesto a false al parar la música.
-      Serial.println("Aquí no debería de entrar"); //Pero entra
-      unsigned long temporizador = 60000 + millis(); // Temporizador para que no suene la musica todo el minuto y luego se duerma
-      while (temporizador > millis()){
-        delay(10);
-      }
-      dormir_tiempo();
-    }
-  } else{ // Esto ocurrirá cuando se inicie la primera vez o cuando se despierte por timer
+  if (wakeup_reason != ESP_SLEEP_WAKEUP_EXT0){ // Esto ocurrirá cuando se inicie la primera vez o cuando se despierte por timer
     pref.begin("Configuration",true);
     bool configSaved = pref.getBool("config_saved", false);
     if (configSaved){
@@ -49,6 +30,7 @@ void setup() {
       Serial.print(hora_morning);
       Serial.print(" ");
       Serial.println(minuto_morning);
+      // Aquí puede que necesite un delay
       chequeo(); // Si hay configuración, miro si me toca sonar y sino me duermo.
       dormir_tiempo();
     } else { // Esto solo debería de hacerlo la primera vez que arranque
@@ -57,11 +39,23 @@ void setup() {
       config = true;
       timeoutCP.start();
     }
+    chequeo_hora.update(); // Este se pone aquí para en caso de ser despertado por botón y que no se haga nada durante los 10 minutos de espera para dormir, pueda sonar la alarma.
+    pref.end();
   } 
-  pref.end();
+  t_pulsado = millis();
 }
 
-void loop() {
+void loop() { // Al loop solo voy a llegar si no me despierto por timer es decir: si es la primera vez que arranco o si quiero entrar en modo config.
+  // Botón soltado
+  if ((millis() - t_pulsado > 3000) && (digitalRead(BUTTON) == LOW)){
+    intFlag = true; // Porque??
+    modoConfig();
+    WiFi_setup(); // Se vuelve a activar la WiFi
+    config = true;  // Activamos modo config
+    timeoutCP.start();
+  }
+  if (digitalRead(BUTTON) == LOW) timer = millis(); // Se actualiza timer cada vez que se pulse.
+  if (!config && millis() - timer > 60000) dormir_tiempo(); // Si después de un minuto no se hace nada, vuelve a dormirse
   if (config){
     dnsServer.processNextRequest();
     server.handleClient();
@@ -69,4 +63,7 @@ void loop() {
   // Chequeo de los timers
   apagarAP.update();
   timeoutCP.update();
+  chequeo_hora.update();
 }
+
+
